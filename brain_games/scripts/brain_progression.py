a30801b690e7604792c274f6a5ef208ed895c547
Mark Rakhmanov <BRODER1CK@mail.ru>
2022-11-16 22:51:25 +0300

    modified scripts

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-from brain_games.info import info
+from brain_games.engine import engine
 from brain_games.games import progression
 
 
 def main():
-    info(progression)
+    engine(progression)
 
 
 if __name__ == '__main__':
