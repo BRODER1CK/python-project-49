7363e9513698c44f24853ec76fabb45c66db5ce8
Mark Rakhmanov <BRODER1CK@mail.ru>
2022-11-16 22:49:58 +0300

    modified calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,5 +1,6 @@
 from random import randint
 from random import choice
+import operator
 
 
 TASK = "What is the result of the expression?"
@@ -9,12 +10,12 @@ def game():
     operators = ["-", "+", "*"]
     num1 = randint(1, 100)
     num2 = randint(1, 100)
-    operator = choice(operators)
-    question = f"{num1} {operator} {num2}"
-    if operator == "-":
-        result = num1 - num2
-    elif operator == "+":
-        result = num1 + num2
-    elif operator == "*":
-        result = num1 * num2
+    operators = choice(operators)
+    question = f"{num1} {operators} {num2}"
+    if operators == "-":
+        result = operator.sub(num1, num2)
+    elif operators == "+":
+        result = operator.add(num1, num2)
+    elif operators == "*":
+        result = operator.mul(num1, num2)
     return question, str(result)
